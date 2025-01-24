21580d9bfe992ed1d1e2326ae47477f2e12e574f
João Gabriel <jg7607221@gmail.com>
2025-01-24 02:06:18 -0300

    Update Projeto.org.py

--- Projeto.org.py
+++ Projeto.org.py
@@ -1,15 +1,27 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("Projeto.org")
+root.title("Interface Dark Mode")
 root.geometry("800x600")  # Tamanho da janela
 
 # Definindo a cor de fundo da janela para um tom escuro
 root.configure(bg="#2e2e2e")
 
 # Criando um label com texto claro
-label = tk.Label(root, text="Bem-vindo ao Projeto.org! Voce é um dos criadores", fg="white", bg="#2e2e2e", font=("Arial", 14))
-label.pack(pady=50)
+#label = tk.Label(root, text="Bem-vindo ao Projeto.org! Voce é um dos criadores", fg="white", bg="#2e2e2e", font=("Arial", 14))
+#label.place(x=50, y=50)
+
+button = tk.Button(root, text="Botão 1", fg="white", bg="#3b3b3b", font=("Arial", 12))
+button.place(x=50, y=25)
+
+button = tk.Button(root, text="Botão 2", fg="white", bg="#3b3b3b", font=("Arial", 12))
+button.place(x=125, y=25)
+
+button = tk.Button(root, text="Botão 3", fg="white", bg="#3b3b3b", font=("Arial", 12))
+button.place(x=200, y=25)
+
+button = tk.Button(root, text="Botão 4", fg="white", bg="#3b3b3b", font=("Arial", 12))
+button.place(x=275, y=25)
 
 # Iniciando o loop da interface
 root.mainloop()
