66eb409767410dd8be936808f1d33ce762b1e061
João Gabriel <jg7607221@gmail.com>
2025-01-24 02:14:59 -0300

    Update Projeto.org.py

--- Projeto.org.py
+++ Projeto.org.py
@@ -1,5 +1,3 @@
-import tkinter as tk
-
 root = tk.Tk()
 root.title("Projeto.org")
 root.geometry("800x600")  # Tamanho da janela
@@ -11,17 +9,21 @@ root.configure(bg="#2e2e2e")
 #label = tk.Label(root, text="Bem-vindo ao Projeto.org! Voce é um dos criadores", fg="white", bg="#2e2e2e", font=("Arial", 14))
 #label.place(x=50, y=50)
 
-button = tk.Button(root, text="Botão 1", fg="white", bg="#3b3b3b", font=("Arial", 12))
+button = tk.Button(root, text="Botão 1", fg="white", bg="#3b3b3b", font=("Arial", 16, "bold"))
 button.place(x=50, y=25)
 
-button = tk.Button(root, text="Botão 2", fg="white", bg="#3b3b3b", font=("Arial", 12))
-button.place(x=125, y=25)
+button = tk.Button(root, text="Botão 2", fg="white", bg="#3b3b3b", font=("Arial", 16, "bold"))
+button.place(x=175, y=25)
 
-button = tk.Button(root, text="Botão 3", fg="white", bg="#3b3b3b", font=("Arial", 12))
-button.place(x=200, y=25)
+button = tk.Button(root, text="Botão 3", fg="white", bg="#3b3b3b", font=("Arial", 16, "bold"))
+button.place(x=300, y=25)
 
-button = tk.Button(root, text="Botão 4", fg="white", bg="#3b3b3b", font=("Arial", 12))
-button.place(x=275, y=25)
+button = tk.Button(root, text="Botão 4", fg="white", bg="#3b3b3b", font=("Arial", 16, "bold"))
+button.place(x=425, y=25)
+
+
 
 # Iniciando o loop da interface
 root.mainloop()
+
+
